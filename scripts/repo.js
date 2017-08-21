222d1d7944fdbb1e430c4067dda66427d4b28129
dan@saucelabs.com <dan@saucelabs.com>
2017-08-21 15:18:58 -0700

    Added multiple language support

--- scripts/repo.js
+++ scripts/repo.js
@@ -6,6 +6,9 @@ import nodeFS from 'fs';
 import path from 'path';
 import unzip from 'unzip';
 import B from 'bluebird';
+import Handlebars from 'handlebars';
+
+const LANGUAGES = ['en', 'cn'];
 
 async function unzipStream (readstream, pathToUnzipped) {
   return new B((resolve, reject) => {
@@ -19,6 +22,7 @@ async function unzipStream (readstream, pathToUnzipped) {
 
 async function getRepoDocs(owner, repo, branch='master') {
   try {
+    // Pull down a zipball of Appium repository
     const github = new Github({
         Promise: require('bluebird')
     });
@@ -30,6 +34,8 @@ async function getRepoDocs(owner, repo, branch='master') {
       },
       encoding: null,
     });
+
+    // Unzip it to a temporary directory
     const tempdir = await tempDir.openDir();
     const pathToZip = path.resolve(tempdir, 'appium.zip');
     await fs.writeFile(pathToZip, archive);
@@ -43,10 +49,6 @@ async function getRepoDocs(owner, repo, branch='master') {
     const pathToAppiumFiles = await fs.readdir(pathToUnzipped);
     const pathToDocs = path.resolve(pathToUnzipped, pathToAppiumFiles[0], 'docs');
 
-    // Copy the mkdocs yml to the directory
-    console.log('!!', pathToDocs);
-    await fs.copyFile(path.resolve(__dirname, '..', 'mkdocs.yml'), path.resolve(pathToDocs, 'mkdocs.yml'));
-
     return pathToDocs;
   } catch(e) {
     console.error('Could not download repo archive. ', e.message);
@@ -54,11 +56,24 @@ async function getRepoDocs(owner, repo, branch='master') {
 };
 
 async function buildDocs (pathToDocs) {
-  const pathToBuildDocsTo = path.resolve(__dirname, '..', 'docs');
-  await exec('mkdocs', ['build', '--site-dir', pathToBuildDocsTo], {
-    cwd: pathToDocs,
-  });
-  await fs.copyFile(pathToBuildDocsTo, path.resolve(__dirname, '..', '_site', 'docs'));
+  const mkdocsTemplate = Handlebars.compile(await fs.readFile(path.resolve(__dirname, '..', 'mkdocs.yml'),  'utf8'));
+
+  // Build the MkDocs for each language
+  for (let language of LANGUAGES) {
+    //await fs.copyFile(path.resolve(__dirname, '..', 'mkdocs.yml'), path.resolve(pathToDocs, language, 'mkdocs.yml'));
+    console.log('MkDOCS yml', mkdocsTemplate({language}));
+    await fs.writeFile(path.resolve(pathToDocs, 'mkdocs.yml'), mkdocsTemplate({language}));
+    const pathToBuildDocsTo = path.resolve(__dirname, '..', 'docs', language);
+    console.log("Building documents to", pathToDocs);
+    await exec('mkdocs', ['build', '--site-dir', pathToBuildDocsTo], {
+      cwd: pathToDocs,
+    });
+
+    // Copy to _site (TODO: How do we make Jekyll do this automatically?)
+    const docsSubdirectory = path.resolve(__dirname, '..', '_site', 'docs', language);
+    await mkdirp(path.resolve(__dirname, docsSubdirectory));
+    await fs.copyFile(pathToBuildDocsTo, docsSubdirectory);
+  }
 };
 
 (async () => {
