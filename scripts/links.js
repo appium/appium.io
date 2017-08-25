1445f005cc6e7be6152aded0a50f077ecb6b137b
Dan Graham <dan@saucelabs.com>
2017-08-25 11:12:32 -0700

    Remap .md links to .html (#94)
    * Remap .md links to .html
      * E.g.: `docs/en/appium-setup/platform-support.md` remaps to `docs/en/appium-setup/platform-support/index.html`
    * Absolute URL's are untouched

---
+++ scripts/links.js
@@ -0,0 +1,41 @@
+import jQuery from 'jquery';
+import { jsdom } from 'jsdom';
+import path from 'path';
+import replaceExtension from 'replace-ext';
+import isAbsoluteUrl from 'is-absolute-url';
+
+const $ = jQuery(jsdom().defaultView);
+
+/**
+ * Markdown doesn't render absolute links to markdown files (see https://github.com/mkdocs/mkdocs/issues/1172)
+ * 
+ * @param {String} html 
+ */
+export function reassignMarkdownLink (html) {
+  const jqHTML = $(html);
+  const anchorTags = jqHTML.find('a');
+  anchorTags.each((index, tag) => {
+    const anchorTag = $(tag);
+    const href = anchorTag.attr('href');
+
+    if (href && !isAbsoluteUrl(href)) {
+      const ext = path.extname(href);
+      if (ext === '.md') {
+        anchorTag.attr('href', `${replaceExtension(href, '')}/index.html`);
+      }
+    }
+  });
+  return jqHTML.html();
+}
+
+/**
+ * Takes a document, parses out the body and then remaps anchor tags to point to HTML links
+ * @param {*} htmlDocString 
+ */
+export function reassignMarkdownLinkDocument (htmlDocString) {
+  const body = '<div id="body-mock">' + htmlDocString.replace(/^[\s\S]*<body.*?>|<\/body>[\s\S]*$/ig, '') + '</div>';
+  const newBody = reassignMarkdownLink(body);
+  const bodyStart = htmlDocString.indexOf('<body>') + '<body>'.length;
+  const bodyEnd = htmlDocString.indexOf('</body>');
+  return htmlDocString.substr(0, bodyStart) + newBody + htmlDocString.substr(bodyEnd, htmlDocString.length - bodyEnd);
+}
---
+++ test/links-specs.js
@@ -0,0 +1,28 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { reassignMarkdownLink } from '../scripts/links';
+
+chai.use(chaiAsPromised);
+chai.should();
+
+describe('links.js', function () {
+  describe('.reassignMarkdownLink', function () {
+    it('should change extension of absolute paths from .md to html', function () {
+      reassignMarkdownLink(`<div><a href="/docs/en/path/to/hello-world.md"></a></div>`)
+        .should.equal(`<a href="/docs/en/path/to/hello-world/index.html"></a>`);
+    });
+    it('should ignore absolute URL paths', function () {
+      const markup = `<div><a href="http://example.com/docs/en/path/to/hello-world.md"></a></div>`;
+      reassignMarkdownLink(markup)
+        .should.equal(`<a href="http://example.com/docs/en/path/to/hello-world.md"></a>`);
+    });
+    it('should change extensions of relative paths', function () {
+      reassignMarkdownLink(`<div><div><a href="docs/en/path/to/hello-world.md"></a></div></div>`)
+        .should.equal(`<div><a href="docs/en/path/to/hello-world/index.html"></a></div>`);
+    });
+    it('should change extensions of multiple absolute paths from .md to html', function () {
+      reassignMarkdownLink(`<div><div><a href="/docs/en/path/to/hello-world.md"><a href="foo-bar.md"></a></div></div>`)
+        .should.equal(`<div><a href="/docs/en/path/to/hello-world/index.html"></a><a href="foo-bar/index.html"></a></div>`);
+    });
+  });
+});
