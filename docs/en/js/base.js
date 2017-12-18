1ac49b7996ece7118ffc3cd9d5cedb5cfc9e965f
Dan Graham <dan@saucelabs.com>
2017-12-18 09:43:20 -0800

    Merge changes from staging into main branch (#112)
    * Don't ignore docs/ folder
    
    * Fix broken link
    
    * Doc generation
    
    * Make max-width for top bar lower
    
    * Top bar wasn't fitting enough menus, so lowered the max-width for when it shrinks down to mobile menu
    * Also added `bundle exec jekyll build` to build script
    
    * Update docs
    
    * Documentation fixes
    
    * Fixed broken links
    
    * Added npm test to Travis build
    * Fixed link remapper so that it accounts for hashes
    
    * CSS fix
    
    * Updated docs

--- docs/en/js/base.js
+++ docs/en/js/base.js
@@ -16,5 +16,12 @@ $("li.disabled a").click(function() {
     event.preventDefault();
 });
 
-
-
+/* handle menu/submenu toggling */
+$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+  // Avoid following the href location when clicking
+  event.preventDefault();
+  // Avoid having the menu to close when clicking
+  event.stopPropagation();
+  // toggle the closest dropdown
+  $(this).closest(".dropdown-submenu").toggleClass("open");
+});
